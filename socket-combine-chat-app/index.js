a092450c3d6eb2b12ebae09ee89712211406f573
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-12-18 07:14:58 +0600

    Tutorial 17: Initialized 'socket-combine-group-chat' directory

---
+++ socket-combine-chat-app/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const http = require("http");
+
+const expressServer = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+expressServer.listen("3000", () => {
+  console.log("Server Run @ 3000");
+});
