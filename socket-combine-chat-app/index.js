60f8a97940a3dafa721b8cb2faf90da35068a582
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-12-18 15:01:36 +0600

    Tutorial 21: Implemented basic chatting feature

--- socket-combine-chat-app/index.js
+++ socket-combine-chat-app/index.js
@@ -1,20 +1,15 @@
 const express = require("express");
-
 const app = express();
-
 const http = require("http");
-
 const expressServer = http.createServer(app);
-
 const { Server } = require("socket.io");
-
 let io = new Server(expressServer);
 
 io.on("connection", (socket) => {
   console.log(`New user connected ${socket.id}`);
 
   socket.on("chat", (msg) => {
-    console.log({ msg }); 
+    io.emit("chat_transfer", msg);
   });
 });
 
